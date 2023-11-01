f87758947bfc6f7504efbe9b71b4e9cf1687a64a
Stephan Weinhold <stephan.weinhold@gmail.com>
2023-11-01 16:44:37 +0100

    Formatted date.

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("fonts");
 
+  eleventyConfig.addFilter("niceDate", (dateString) => {
+    dateObj = new Date(dateString);
+    return dateObj.toLocaleDateString('utc', { year:"numeric", month:"short", day:"numeric"}) ;
+  });
+
   eleventyConfig.addPlugin(require("./_11ty/optimize-html.js"));
 };
