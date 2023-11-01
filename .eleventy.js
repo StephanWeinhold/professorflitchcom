48fed0fc128bb8874d6f38fe19d9eeb6a4cf1336
Stephan Weinhold <stephan.weinhold@gmail.com>
2023-11-01 14:48:20 +0100

    Included optimize-html plugin.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("fonts");
+
+  eleventyConfig.addPlugin(require("./_11ty/optimize-html.js"));
 };
